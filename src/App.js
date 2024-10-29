d27b4c951dbb27f795f329aa9b77e75f46f399ed
Heba-Rabaya <heba.88da44.600n@gmail.com>
2024-10-29 21:02:52 +0200

    Add App.js
    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+import Header from './components/Header';
+import Menu from './components/Menu';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <Menu />
+        </div>
+    );
+}
+
+export default App;
